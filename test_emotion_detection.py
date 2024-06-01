245a1dccdac0fe75ecd49790d581b7a2ccf0483a
Johana Duchi Tipán <112076346+jduchit@users.noreply.github.com>
2024-06-01 17:37:57 -0500

    Update test_emotion_detection

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,4 +1,4 @@
-import emotion_detection
+from emotiondetection import emotion_detection 
 
 text_to_analyze = "I love this new technology."
 result = emotion_detection.emotion_detector(text_to_analyze)
